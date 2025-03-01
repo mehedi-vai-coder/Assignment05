5133a641efdf184ab86dc06fd6f617bcea09d96c
MD MEHEDI HASAN <md.mehedihasan8n@gmail.com>
2025-03-02 02:58:48 +0600

    fix my file path

--- challange.js
+++ challange.js
@@ -20,3 +20,4 @@ document.getElementById("color-change").addEventListener("click", function () {
     const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16); 
     body.style.backgroundColor = randomColor;
 });
+
